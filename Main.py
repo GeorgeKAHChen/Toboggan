d8ec1fd001b99381414804bc3ba2041a726cf4f6
Kazuki Amakawa <KazukiAmakawa@gmail.com>
2018-05-16 09:03:12 +0800

    Delete Main.py

--- Main.py
+++
@@ -1,8 +0,0 @@
-############################################################
-#
-#		Toboggan Image Segmentation Algorithm
-#		Copyright(c) KazukiAmakawa, all right reserved.
-#		Main.py
-#
-############################################################
-
